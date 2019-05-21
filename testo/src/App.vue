e35d0852a9f6af7c60b9fd0cd3641d2a10bd3d2e
Himmelsora <sora.20030217@gmail.com>
2019-05-22 00:51:02 +0900

    first commit

--- testo/src/App.vue
+++ testo/src/App.vue
@@ -1,7 +1,45 @@
 <template>
-  <div id="app">
-    <p>
-      サンプル
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{msg}}
     </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
   </div>
 </template>
+
+<script>
+import myheader from './components/myheader'
+
+export default {
+  components: {
+    myheader
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  },
+  created(){
+    fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&COUNTRY=us')
+    .then(response =>{
+      return response.json()
+    })
+    .then(json =>{
+      this.msg = json.postalcodes[0].adminName1
+    })
+    .catch( () =>{
+
+    });
+  }
+}
+</script>
