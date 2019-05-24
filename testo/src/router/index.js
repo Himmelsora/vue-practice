6446405006c4ce061db22489b38acf772c260f23
Himmelsora <sora.20030217@gmail.com>
2019-05-24 12:31:42 +0900

    first commit

--- testo/src/router/index.js
+++ testo/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import page1 from '@/components/page1'
+import page2 from '@/components/page2'
 
 Vue.use(Router)
 
@@ -8,8 +10,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: page1
+    },
+    {
+      path: '/page2',
+      component: page1
     }
   ]
 })
